4f4fb90584ff7257063696ab08ae43988308a01a
kihyeoon <bangion93@gmail.com>
2023-04-21 22:04:03 +0900

    feat: Chart 페이지 컴포넌트 초기 구현 (#3)

--- src/pages/Chart.tsx
+++ src/pages/Chart.tsx
@@ -1,9 +1,41 @@
-function Chart() {
+import styles from "./Chart.module.css";
+import { Album } from "../types/chart.type";
+import ChartItem from "../components/ChartItem";
+import { useEffect, useState } from "react";
+
+const Chart = () => {
+  const [albums, setAlbums] = useState<Album[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    (async () => {
+      const response = await fetch(
+        "https://itunes.apple.com/us/rss/topalbums/limit=100/json"
+      );
+      const data = await response.json();
+      setAlbums(data.feed.entry);
+      setLoading(false);
+    })();
+  }, []);
+
   return (
     <>
-      <h1>Chart</h1>
+      <header>
+        <h1 className={styles.title}>EL Chart: Top Albums</h1>
+      </header>
+      <main className={styles.albumChart}>
+        <ul>
+          {loading ? (
+            <p>Loading...</p>
+          ) : (
+            albums.map((album, index) => (
+              <ChartItem key={index} album={album} />
+            ))
+          )}
+        </ul>
+      </main>
     </>
   );
-}
+};
 
 export default Chart;
